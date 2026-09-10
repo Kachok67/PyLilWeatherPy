d429523fed2d7aec4f5451ec1a951c35f302f57b
Kachok67 <Kachok67@proton.me>
2026-09-10 17:32:10 +0200

    Add user option for custom latitude and longitude
    Added comments

--- app.py
+++ app.py
@@ -12,20 +12,23 @@ except ImportError:
 Error = ""
 
 
-# Font sizes>
+# Font sizes
 PfontSize = 12;
 HfontSize = 16;
 P2fontSize = 10;
 
-
+# Asks user if they would like to use the default Vienna preset or enter custom coordinates
 usroption = input("Would you like to set custom latitude and longtitude? (y/n for Vienna) ")
 if usroption == "y":
+    # Prompts the user to enter custom values
     latitude = input("Please enter a latitude: ")
     longtitude = input("Please enter a longtitude: ")
 else:
+    # Defaults
     latitude = 48.20849
     longtitude = 16.37208
 
+# Error Checking subprocess (Checks every 200 Milliseconds to minimize perfomance impact)
 def CheckErrors():
     while True:
         global Error  
@@ -33,15 +36,19 @@ def CheckErrors():
             print(Error)
             os._exit(1)
             exit()
-        time.sleep(0.1)
+        time.sleep(0.2)
 
+# Initialises and starts the error checking process
 ErrorThread = threading.Thread(target=CheckErrors)
 ErrorThread.start()
 
+# Url to fetch Jason from
 url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longtitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
 
 try:
     response = urlopen(url).read().decode()
+
+# Probably a internet connection problem
 except URLError:
     Error = "Data couldnt be fetched. Check your internet connection"
 
@@ -52,10 +59,12 @@ try:
 except json.JSONDecodeError:
     Error = "There was an error decoding the json"
 
+# Logs...
 with open("log", "w") as logfile:
     json.dump(data, logfile, indent=3)
 
 try:
+    # sets variables and prints data into terminal
     print(f"The current temperature is {data['current']['temperature_2m']} Celcius")
     print(f"The current wind speed is {data['current']['wind_speed_10m']} km/h")
     print(f"The current time is {data['current']['time']}")
@@ -78,6 +87,7 @@ try:
 
     strongestWind = f"Strongest wind today: {max(winds)} km/h"
 
+    # Humidity (todo)
     # weather_code = data["current"]["relative_humidity_2m"]
 
     # if weather_code in [51, 53, 55, 61, 63, 65, 80, 81, 82]:
@@ -89,7 +99,7 @@ except KeyError:
     Error = "Some expected fields are missing in the json"
 
 
-
+# Creates UI and Object
 ui = UI.UI()
 ui.CreateUI(latitude, longtitude, currenttimeText, tempText, highestTempText, lowestTempText, windspeedText, strongestWind ,PfontSize, HfontSize, P2fontSize)
 ui.update()
